b4eb5e5dfc090c007fdc8b1a6f2716766c53ab86
Anthony Brummett <brummett@gmail.com>
2014-02-05 19:36:54 -0600

    New test fixture with step test

--- t/02-step.t
+++ t/02-step.t
@@ -1,52 +1,30 @@
-#!/usr/bin/env perl -d:CommonDB
 use strict;
 use warnings; no warnings 'void';
-use Test::More tests => 16;
 
-6;
-foo();
-8;
-sub foo {
-    10;
-}
+use lib 'lib';
+use lib 't/lib';
+use Devel::CommonDB::TestRunner;
 
-
-package TestDB;
-
-use base 'Devel::CommonDB';
-
-my @tests;
-BEGIN {
-    @tests = (
-        { package => 'main', subroutine => 'MAIN', line => 6, filename => __FILE__ },
-        { package => 'main', subroutine => 'MAIN', line => 7, filename => __FILE__ },
-        { package => 'main', subroutine => 'main::foo', line => 10, filename => __FILE__ },
-        { package => 'main', subroutine => 'MAIN', line => 8, filename => __FILE__ },
-    );
-    
-    TestDB->attach();
-}
-
-sub notify_stopped {
-    my($class, $loc) = @_;
-    
-    my $next_test = shift @tests;
-    exit unless $next_test;
-
-    Test::More::note('Testing line '.$next_test->{line});
-
-    foreach my $prop ( keys %$next_test ) {
-        Test::More::is($loc->$prop, $next_test->{$prop}, "Location $prop");
-    }
-}
-
-sub poll {
-    return scalar(@tests);
-}
-
-sub idle {
-    TestDB->step();
-    1;
-}
+run_test(
+    5,
+    sub {
+        $DB::single=1;
+        12;
+        foo();
+        14;
+        sub foo {
+            16;
+        }
+    },
+    loc(line => 12),
+    'step',
+    loc(line => 13),
+    'step',
+    loc(subroutine => 'main::foo', line => 16),
+    'step',
+    loc(line => 14),
+    'step',
+    'at_end',
+);
 
 
---
+++ t/lib/Devel/CommonDB/TestRunner.pm
@@ -0,0 +1,152 @@
+package Devel::CommonDB::TestRunner;
+
+use Devel::CommonDB;
+use Devel::CommonDB::Location;
+use base 'Devel::CommonDB';
+use Carp;
+
+use Exporter qw(import);
+our @EXPORT = qw(run_test loc);
+
+sub _is_in_test_program {
+    no warnings 'uninitialized';
+    return $ARGV[0] eq '--test';
+}
+
+my $PKG = __PACKAGE__;
+our $at_end = 1;
+sub run_test {
+    run_in_debugger() unless _is_in_test_program();
+
+    my $plan = shift;
+    my $program = shift;
+    my @tests = @_;
+
+    eval "use Test::More";
+    Carp::croak("Can't use Test::More: $@") if $@;
+    plan(tests => $plan);
+
+
+    my $db = bless \@tests, $PKG;
+    $db->attach();
+    {
+        local($at_end) = 0;
+        $program->();
+    }
+
+}
+
+sub loc {
+    my %params = @_;
+
+    defined($params{subroutine}) || do { $params{subroutine} = 'ANON' };
+    defined($params{filename}) || do { $params{filename} = (caller)[1] };
+    defined($params{package}) || do { $params{package} = 'main' };
+    return Devel::CommonDB::Location->new(%params);
+}
+
+sub notify_stopped {
+    my($db, $loc) = @_;
+    #printf("stopped at %s:%d\n", $loc->filename, $loc->line);
+
+    COMMAND_LOOP:
+    while( my $next_test = shift @$db ) {
+
+        if (ref($next_test) eq 'CODE') {
+            $next_test->($db, $loc);
+
+        } elsif ($next_test->isa('Devel::CommonDB::Location')) {
+            _compare_locations($db, $loc, $next_test);
+
+        } elsif (! ref($next_test)) {
+            $db->$next_test();
+
+        } else {
+            Carp::croak('Unknown test type '.ref($next_test));
+        }
+    }
+
+    if (! @$db and ! $at_end) {
+        ok(0, sprintf('Ran out of tests before reaching done, at %s:%d',
+                        $loc->filename, $loc->line));
+        exit;
+    } elsif (@$db and $at_end) {
+        ok(0, 'Test code ended with '.scalar(@$db).' tests remaining');
+    }
+}
+
+sub notify_program_exit {
+    my $db = shift;
+    if (@$db) {
+        ok(0, "program exit before ",scalar(@$db)," commands consumed");
+    }
+}
+
+sub _compare_locations {
+    my($db, $got_loc, $expected_loc) = @_;
+
+    my @compare = (
+        sub {
+                my $expected_sub = $expected_loc->subroutine;
+                return ($expected_sub eq 'ANON')
+                        ? $got_loc->subroutine =~ m/__ANON__/
+                        : $got_loc->subroutine eq $expected_sub;
+            },
+        sub { return $expected_loc->package eq $got_loc->package },
+        sub { return $expected_loc->line == $got_loc->line },
+        sub { return $expected_loc->filename eq $got_loc->filename },
+    );
+
+    my $report_test; $report_test = sub {
+        Test::More::ok(shift, sprintf('Expected location %s:%d got %s:%d',
+                                    $expected_loc->filename, $expected_loc->line,
+                                    $got_loc->filename, $got_loc->line));
+        $report_test = sub {}; # only report the error once
+    };
+
+    foreach my $compare ( @compare ) {
+        unless ( $compare->() ) {
+            $report_test->(0);
+        }
+    }
+    $report_test->(1);
+}
+            
+
+sub step {
+    my $db = shift;
+    $db->SUPER::step();
+    last COMMAND_LOOP;
+}
+
+sub done {
+    my $db = shift;
+    $db->user_requested_exit();
+    $db->continue;
+    last COMMAND_LOOP;
+}
+
+sub at_end {
+    my $db = shift;
+    Test::More::ok($at_end, 'finished');
+}
+    
+
+
+sub run_in_debugger {
+    my $pid = fork();
+    if ($pid) {
+        waitpid($pid, 0);
+        Carp::croak("Child test program exited with status $?") if $?;
+        exit;
+
+    } elsif (defined $pid) {
+        exec($^X, '-Ilib', '-It/lib', '-d:CommonDB::TestRunner', $0, '--test');
+        Carp::croak("Exec test program failed: $!");
+
+    } else {
+        Carp::croak("Fork test program failed: $!");
+    }
+}
+
+1;
