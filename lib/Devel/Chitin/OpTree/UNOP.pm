26e24c66c158e1b6f523f995255471dda56eee8b
Anthony Brummett <brummett@gmail.com>
2015-12-17 16:27:33 -0600

    get the scalar_ref_assignment test working after the changes to pp_null

--- lib/Devel/Chitin/OpTree/UNOP.pm
+++ lib/Devel/Chitin/OpTree/UNOP.pm
@@ -23,6 +23,13 @@ sub pp_null {
     $self->$bounce();
 }
 
+# Normally, pp_list is a LISTOP, but this happens when a pp_list is turned
+# into a pp_null by the optimizer, and it has one child
+sub pp_list {
+    my $self = shift;
+    $self->first->deparse;
+}
+
 sub _ex_name {
     my $self = shift;
     if ($self->op->name eq 'null') {
