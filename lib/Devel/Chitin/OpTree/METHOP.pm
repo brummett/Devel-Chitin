3661d7cd5c45bf41b0348dad51f32f8327ec5cbe
Anthony Brummett <brummett@gmail.com>
2016-03-29 10:20:31 -0500

    pp_method_named is a METHOP in Perl 5.22

--- lib/Devel/Chitin/OpTree/METHOP.pm
+++ lib/Devel/Chitin/OpTree/METHOP.pm
@@ -1,9 +1,14 @@
 package Devel::Chitin::OpTree::METHOP;
-use base 'Devel::Chitin::OpTree';
+use base 'Devel::Chitin::OpTree::UNOP';
 
 use Devel::Chitin::Version;
 
 use strict;
 use warnings;
+
+sub pp_method_named {
+    my $self = shift;
+    $self->op->meth_sv->PV;
+}
 
 1;
