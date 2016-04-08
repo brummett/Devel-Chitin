brummett/Devel-Chitin

commit 36d57aebbcdad4ac0853733beee1ac2a93b99804

deparse aliasing lists

lib/Devel/Chitin/OpTree/UNOP.pm
@@ -64,10 +64,11 @@ sub pp_refgen {
         my $deparser = Devel::Chitin::OpTree->build_from_location($subref->object_2svref);
         'sub { ' . $deparser->deparse . ' }';
     } else {
-        '\\' . $first->deparse;
+        '\\' . $first->deparse(in_refgen => 1);
     }
 }
 *pp_srefgen = \&pp_refgen;
+*pp_lvref = \&pp_refgen;
 
 sub pp_rv2sv { '$' . shift->first->deparse }
 sub pp_rv2av { '@' . shift->first->deparse }

lib/Devel/Chitin/OpTree.pm
@@ -471,6 +471,13 @@ sub pp_eof {
         : 'eof';
 }
 
+sub pp_lvavref {
+    my $self = shift;
+    '(' . $self->_padname_sv->PV . ')';
+    #$self->_padname_sv->PV;
+}
+
+
 # file test operators
 # These actually show up as UNOPs (usually) and SVOPs (-X _) but it's
 # convienent to put them here in the base class
@@ -657,6 +664,31 @@ sub _deparse_for_loop {
     my $cont = $cont_op->deparse;
 
     "for ($init; $test; $cont) " . $body_op->deparse;
+}
+
+# Return true if this op is the inner list on the right of
+# \(@a) = \(@b)
+# The optree for this construct looks like:
+# aassign
+#   ex-list
+#     pushmark
+#     refgen
+#       ex-list <-- Return true here
+#         pushmark
+#         padav/gv
+#   ex-list
+#     pushmark
+#     ex-refgen
+#       ex-list <-- return true here
+#           pushmark
+#           lvavref
+sub is_list_reference_alias {
+    my $self = shift;
+
+    return $self->is_null
+            && $self->_ex_name eq 'pp_list'
+            && $self->parent->op->name eq 'refgen'
+            && $self->last->is_array_container;
 }
 
 sub _quote_sv {

lib/Devel/Chitin/OpTree/BINOP.pm
@@ -79,7 +79,10 @@ sub pp_list {
 
     my $contents = $self->last->deparse;
 
-    if ($self->last->is_scalar_container) {
+    if ($self->last->is_scalar_container
+        or
+        $self->is_list_reference_alias
+    ) {
         "(${contents})";
 
     } else {

lib/Devel/Chitin/OpTree/LISTOP.pm
@@ -106,6 +106,13 @@ sub pp_hslice {
 }
 *pp_kvhslice = \&pp_hslice;
 
+sub pp_lvrefslice {
+    my $self = shift;
+    $self->last->op->name =~ m/av$/
+        ? $self->pp_aslice
+        : $self->pp_hslice;
+}
+
 my %aslice_hslice_allowed_ops = map { $_ => 1 } qw( padav padhv rv2av rv2hv );
 sub _aslice_hslice_builder {
     my($self, $open_paren, $close_paren) = @_;

t/20-optree.t
@@ -1155,6 +1155,11 @@ subtest 'perl-5.22' => sub {
                                     q(\$array[1] = \$a;),
                                     q(my %hash = (1, 1);),
                                     q(\$hash{'1'} = \$b)),
+        listref_alias => join("\n", q(my($a, $b, @array);),
+                                    q(\@array[1, 2] = (\$a, \$b);),
+                                    q[\(@array) = (\$a, \$b)]),
+        alias_whole_array => join("\n", q(my(@array, @ar2);),
+                                        q[\(@array) = \(@ar2)]),
     );
 };
 

commit c7008b03b555cd8c159869e627b816abdde87509

Deparse \@list properly on 5.18.* and earlier
Older Perls' Optrees make this deparse as \(@list).  This initially broke
with 36d57aeb when adding deparsing for aliasing lists with \(@list) = \(@other).

lib/Devel/Chitin/OpTree/UNOP.pm
@@ -63,6 +63,17 @@ sub pp_refgen {
         my $subref = $self->_padval_sv($anoncode->op->targ);
         my $deparser = Devel::Chitin::OpTree->build_from_location($subref->object_2svref);
         'sub { ' . $deparser->deparse . ' }';
+
+    } elsif ($first->is_null
+             and $first->_ex_name eq 'pp_list'
+             and @{$first->children} == 2
+             and $first->children->[-1]->is_array_container
+             and $first->children->[-1]->op->flags & B::OPf_REF
+    ) {
+        # This catches the case of \@list.  Falling through to the default
+        # case, it'll deparse as \(@list)
+        '\\' . $first->children->[-1]->deparse;
+
     } else {
         '\\' . $first->deparse;
     }